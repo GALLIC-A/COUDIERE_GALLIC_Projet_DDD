1aba7940c5ac292eb3d8fa2db569c98a99c713de
Axel <axoul@test.test>
2025-04-11 17:18:31 +0200

    Ajout des endpoints et serializers pour TRAFFIC et MUSIQUE

--- Backend/apps/api/urls.py
+++ Backend/apps/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import get_tests, search_profession
+from .views import get_tests, search_profession, get_traffic, get_musique
 
 urlpatterns = [
     path('tests/', get_tests, name='get_tests'),
-    path('search_profession/', search_profession, name='search_profession'),
+    path('profession/', search_profession, name='search_profession'),
+    path('traffic/', get_traffic, name='get_traffic'),
+    path('musique/', get_musique, name='get_musique'),
 ]
--- Backend/apps/api/views.py
+++ Backend/apps/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Test, Profession
-from .serializers import ProfessionSerializer
+from .models import Test, Profession, Traffic, Musique
+from .serializers import ProfessionSerializer, TrafficSerializer, MusiqueSerializer
 
 @api_view(['GET'])
 def get_tests(request):
@@ -15,4 +15,16 @@ def search_profession(request):
     name = request.GET.get('name', '')
     professions = Profession.objects.find_by_name(name)
     serializer = ProfessionSerializer(professions, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_traffic(request):
+    traffics = Traffic.objects.all()
+    serializer = TrafficSerializer(traffics, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_musique(request):
+    musiques = Musique.objects.all()
+    serializer = MusiqueSerializer(musiques, many=True)
     return Response(serializer.data)
